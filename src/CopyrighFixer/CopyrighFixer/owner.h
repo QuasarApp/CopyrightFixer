f1818d90c1bfd7435a65482bd275b50e6622015a
Igor loschinin <igor.loschinin2014@yandex.ru>
2021-04-17 22:25:35 +0300

    Update src/CopyrighFixer/CopyrighFixer/owner.h
    Co-authored-by: Andrei Yankovich <EndrIIMail@gmail.com>

--- src/CopyrighFixer/CopyrighFixer/owner.h
+++ src/CopyrighFixer/CopyrighFixer/owner.h
@@ -5,6 +5,13 @@
 //# of this license document, but changing it is not allowed.
 //#
 
+//#
+//# Copyright (C) 2021-2021 QuasarApp.
+//# Distributed under the lgplv3 software license, see the accompanying
+//# Everyone is permitted to copy and distribute verbatim copies
+//# of this license document, but changing it is not allowed.
+//#
+
 #ifndef OWNER_H
 #define OWNER_H
 
