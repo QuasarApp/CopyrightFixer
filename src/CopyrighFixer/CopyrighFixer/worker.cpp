a53c88539e6e01b518ea84c57def668c2670fb63
IgorekLoschinin <igor.loschinin2014@yandex.ru>
2021-04-22 22:30:03 +0300

    ref #29 Added main class.

---
+++ src/CopyrighFixer/CopyrighFixer/worker.cpp
@@ -0,0 +1,23 @@
+//#
+//# Copyright (C) 2021-2021 QuasarApp.
+//# Distributed under the lgplv3 software license, see the accompanying
+//# Everyone is permitted to copy and distribute verbatim copies
+//# of this license document, but changing it is not allowed.
+//#
+
+#include "config.h"
+#include "worker.h"
+#include <quasarapp.h>
+
+
+
+namespace CopyrighFixer {
+Worker::Worker() {
+
+}
+
+bool Worker::runProcessing() {
+    return 1;
+}
+
+};
---
+++ src/CopyrighFixer/CopyrighFixer/worker.h
@@ -0,0 +1,34 @@
+//#
+//# Copyright (C) 2021-2021 QuasarApp.
+//# Distributed under the lgplv3 software license, see the accompanying
+//# Everyone is permitted to copy and distribute verbatim copies
+//# of this license document, but changing it is not allowed.
+//#
+
+#ifndef WORKER_H
+#define WORKER_H
+
+#include "CopyrighFixer_global.h"
+
+
+namespace CopyrighFixer {
+
+/**
+ * @brief The Worker class that will be control all work process.
+ */
+class CopyrighFixer_EXPORT Worker {
+public:
+    Worker();
+
+    /**
+     * @brief runProcessing It is main method for control of all parsing process.
+     * @return
+     */
+    bool runProcessing();
+
+
+};
+
+}
+
+#endif // WORKER_H
