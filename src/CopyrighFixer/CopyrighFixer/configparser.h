a40da125555225d8866c108258c8396aa0f863a0
IgorekLoschinin <igor.loschinin2014@yandex.ru>
2021-04-24 22:26:02 +0300

    ref #49 Added class configParser.

---
+++ src/CopyrighFixer/CopyrighFixer/configparser.h
@@ -0,0 +1,34 @@
+//#
+//# Copyright (C) 2021-2021 QuasarApp.
+//# Distributed under the lgplv3 software license, see the accompanying
+//# Everyone is permitted to copy and distribute verbatim copies
+//# of this license document, but changing it is not allowed.
+//#
+
+#ifndef CONFIGPARSER_H
+#define CONFIGPARSER_H
+
+#include "CopyrighFixer_global.h"
+#include "config.h"
+
+namespace CopyrighFixer {
+
+/**
+ * @brief The ConfigParser class
+ */
+class ConfigParser {
+public:
+    ConfigParser();
+
+    /**
+     * @brief parseOptions This a method that will parse all input options.
+     * @return The config object.
+     */
+    const Config &parseOptions() const;
+
+};
+
+}
+
+
+#endif // CONFIGPARSER_H
