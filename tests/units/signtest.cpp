a8b281e48d636394ffd4bc011481fc658e2b42a3
Igor loschinin <igor.loschinin2014@yandex.ru>
2021-05-03 20:36:46 +0300

    Update tests/units/signtest.cpp
    Co-authored-by: Andrei Yankovich <EndrIIMail@gmail.com>

--- tests/units/signtest.cpp
+++ tests/units/signtest.cpp
@@ -23,21 +23,17 @@ void SignTest::test() {
 void SignTest::testJsonObj() {
     QString filename = "signature.json";
 
-    CopyrighFixer::Owner ownerObj;
-    ownerObj.setName("QuasarApp");
-    ownerObj.setTimePoint(QDateTime::currentMSecsSinceEpoch());
+    CopyrighFixer::Sign baseSign = generateRandomSign();
+    
+    QVERIFY(baseSign.toJson(filename));
+    
+    QVERIFY(baseSign.isValid());
+    
+    CopyrighFixer::Sign signFromFile;
+    QVERIFY(signFromFile.fromJson(filename));
+    
+    QVERIFY(signFromFile == baseSign);
 
-    QMap<int, CopyrighFixer::Owner> OwnerMap;
-    OwnerMap.insert(ownerObj.getTimePoint(), ownerObj);
-
-    CopyrighFixer::Signature sign_toJson;
-    sign_toJson.setLicenseTitle("Copyright (C) 2020-2021 QuasarApp.");
-    sign_toJson.setMessage("Distributed under the lgplv3 software license, see the accompany.");
-    sign_toJson.setMapOwners(OwnerMap);
-    sign_toJson.toJson(filename);
-
-    CopyrighFixer::Signature sign_fromJson;
-    sign_fromJson.fromJson(filename);
 
     QVERIFY(sign_fromJson.isValid() == sign_toJson.isValid());
 }
