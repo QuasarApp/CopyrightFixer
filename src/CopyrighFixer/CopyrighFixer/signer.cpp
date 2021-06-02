3d370559fd9aec2fe4acd6d06662a24115e62b42
IgorekLoschinin <igor.loschinin2014@yandex.ru>
2021-06-02 22:27:55 +0300

    ref #60 Added implementation for method checkSign.

--- src/CopyrighFixer/CopyrighFixer/signer.cpp
+++ src/CopyrighFixer/CopyrighFixer/signer.cpp
@@ -7,13 +7,55 @@
 
 #include "signer.h"
 #include "CopyrighFixer/ifilemanager.h"
+#include <QDir>
 
 namespace CopyrighFixer {
 Signer::Signer() {
 
 }
 
-bool Signer::checkSign(const Config &objConf) {
+bool Signer::checkSign(const Config &objConf) {    
+
+    Config currConfig = objConf;
+
+    QDir currentFolder(currConfig.getSrcDir());
+    currentFolder.setFilter(QDir::Dirs | QDir::Files);
+
+    QFileInfoList folderItems(currentFolder.entryInfoList());
+
+    for (auto i_file: folderItems) {
+
+        if (i_file.fileName() == "." || i_file.fileName() == "..") {
+            continue;
+        }
+
+        if (!i_file.isDir()) {
+
+            IFileManager *currFM = searchFileByExt(i_file.suffix());
+            Signature objSing = currConfig.getSignVal();
+
+            if (currFM == nullptr) {
+                return false;
+            }
+
+            bool valRead = currFM->read(i_file.filePath(), objSing);
+            if (!valRead) {
+                return false;
+            }
+
+            currConfig.setSingValue(objSing);
+
+            bool valWrite = currFM->write(i_file.filePath(), objSing);
+            if (!valWrite) {
+                return false;
+            }
+
+        } else {
+            currConfig.setSourceDir(i_file.filePath());
+            checkSign(currConfig);
+        }
+    }
+
     return true;
 }
 
@@ -24,7 +66,6 @@ IFileManager *Signer::searchFileByExt(const QString &extension) {
         if (itemFM && itemFM->isSupport(itemFM->toExtension(extension))) {
             return itemFM;
         }
-
 
     }
     return nullptr;
